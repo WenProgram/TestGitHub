937bcbfe208d9fe9b6d53e3f4212c79d59749ae2
mate <mate@bogon>
2017-12-06 16:26:38 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by mate on 17/12/6.
+//  Copyright © 2017年 mate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
